fc221b4580f32faa1ff0a1f3beba10d4eb724038
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-11 16:49:49 +0200

    Add some elements to contact

--- src/pages/contact-page.js
+++ src/pages/contact-page.js
@@ -1,6 +1,30 @@
 const contactSection = function contactSection(mainElement) {
   const contact = document.createElement("div");
-  contact.innerHTML = "<div>Contact us:</div>";
+
+  const heading = document.createElement("h1");
+  heading.textContent = "Contact us:";
+
+  const personInfo = document.createElement("div");
+  const smallHeading = document.createElement("h2");
+  smallHeading.textContent = "Person";
+  personInfo.appendChild(smallHeading);
+
+
+  const anotherDiv = document.createElement("div");
+  const position = document.createElement("span");
+  position.textContent = "Position: Chef";
+  const tele = document.createElement("span");
+  tele.textContent = "Telephone Number: 111-111-111-111";
+  const email = document.createElement("span");
+  email.textContent = "Email: abc@example.com";
+  anotherDiv.appendChild(position);
+  anotherDiv.appendChild(tele);
+  anotherDiv.appendChild(email);
+  personInfo.appendChild(anotherDiv);
+
+  contact.appendChild(heading);
+  contact.appendChild(personInfo);
+
   mainElement.appendChild(contact);
 };
 
