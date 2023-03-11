f49846f7dd0aedfbdfbad8def52ba2ee4026eac4
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-11 18:59:32 +0200

    Rebuild menu page and using sass to style it

--- src/pages/menu-page.js
+++ src/pages/menu-page.js
@@ -1,6 +1,11 @@
+import "../assets/menu.scss";
+
 const menuSection = function contactSection(mainElement) {
   const menu = document.createElement("div");
-  menu.innerHTML = "<div>Menu:</div>";
+  menu.classList.add("menu-container");
+  const heading = document.createElement("h1");
+  heading.textContent = "Menu";
+  menu.appendChild(heading);
   mainElement.appendChild(menu);
 };
 
