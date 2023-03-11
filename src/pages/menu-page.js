a26ccabcdb83e64a23a4e2a2625dba4b68d71ecc
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-11 19:20:30 +0200

    Build function to generate more menu dishes

--- src/pages/menu-page.js
+++ src/pages/menu-page.js
@@ -5,30 +5,34 @@ const menuSection = function contactSection(mainElement) {
   menu.classList.add("menu-container");
   const heading = document.createElement("h1");
   heading.textContent = "Menu";
-  
-  const menuInfo = document.createElement("div");
-  menuInfo.classList.add("menu-info");
-  const smallHeading = document.createElement("h2");
-  smallHeading.textContent = "Person";
-  menuInfo.appendChild(smallHeading);
 
-  const anotherDiv = document.createElement("div");
-  const description = document.createElement("p");
-  description.textContent = "description";
-  const price = document.createElement("p");
-  price.textContent = "Price:";
-  const image = document.createElement("img");
-  image.src = "";
-  image.alt= "menu dish";
-  anotherDiv.appendChild(description);
-  anotherDiv.appendChild(price);
-  anotherDiv.appendChild(image);
-  menuInfo.appendChild(anotherDiv);
+  const buildMenu = function buildMenu(name, descriptionText, priceValue, ...imageUrl) {
+    const menuInfo = document.createElement("div");
+    menuInfo.classList.add("menu-info");
+    const smallHeading = document.createElement("h2");
+    smallHeading.textContent = name;
+    menuInfo.appendChild(smallHeading);
+
+    const anotherDiv = document.createElement("div");
+    const description = document.createElement("p");
+    description.textContent = descriptionText;
+    const price = document.createElement("p");
+    price.textContent = `Price: ${priceValue}$`;
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.alt = "menu dish";
+    anotherDiv.appendChild(description);
+    anotherDiv.appendChild(price);
+    anotherDiv.appendChild(image);
+    menuInfo.appendChild(anotherDiv);
+
+    return menuInfo;
+  };
 
   menu.appendChild(heading);
-  menu.appendChild(menuInfo);
-  
-  
+  menu.appendChild(buildMenu());
+  menu.appendChild(buildMenu());
+
   mainElement.appendChild(menu);
 };
 
