c363e5f37d24872b90ede6b8bd3bd943942a073f
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-11 19:12:07 +0200

    Add some elements to menu page

--- src/pages/menu-page.js
+++ src/pages/menu-page.js
@@ -5,7 +5,30 @@ const menuSection = function contactSection(mainElement) {
   menu.classList.add("menu-container");
   const heading = document.createElement("h1");
   heading.textContent = "Menu";
+  
+  const menuInfo = document.createElement("div");
+  menuInfo.classList.add("menu-info");
+  const smallHeading = document.createElement("h2");
+  smallHeading.textContent = "Person";
+  menuInfo.appendChild(smallHeading);
+
+  const anotherDiv = document.createElement("div");
+  const description = document.createElement("p");
+  description.textContent = "description";
+  const price = document.createElement("p");
+  price.textContent = "Price:";
+  const image = document.createElement("img");
+  image.src = "";
+  image.alt= "menu dish";
+  anotherDiv.appendChild(description);
+  anotherDiv.appendChild(price);
+  anotherDiv.appendChild(image);
+  menuInfo.appendChild(anotherDiv);
+
   menu.appendChild(heading);
+  menu.appendChild(menuInfo);
+  
+  
   mainElement.appendChild(menu);
 };
 
